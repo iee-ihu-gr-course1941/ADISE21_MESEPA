3927707bc6fc9dd6e63c89406093051ed74fa69d
xenofon pantsos <pantsosxen23@gmail.com>
2021-11-30 18:23:40 +0200

    k

--- quarto.php
+++ quarto.php
@@ -1,5 +1,8 @@
 <?php 
 
+require_once "lib/dbconnect.php";
+require_once "lib/board.php";
+require_once "lib/game.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
@@ -9,7 +12,7 @@ switch ($r=array_shift($request)) {
     case 'board' : 
                 switch ($b=array_shift($request)) {
                         case '':
-                        case null: handle_board($method);
+                        case null: handle_board($method);      
                                 break;
                         case 'piece': handle_piece($method, $request[0]);
                                 break;
@@ -17,7 +20,8 @@ switch ($r=array_shift($request)) {
                                 break;
 			}
 		break;
-    case 'status': if(sizeof($request)==0){
+    case 'status': 
+        if(sizeof($request)==0){
                         show_status();
                    }
 		   else{
@@ -31,7 +35,7 @@ switch ($r=array_shift($request)) {
                 exit;
 }
 
-function handle_board($method,$input) {
+function handle_board($method) {
     if($method=='GET') {
             show_board();
     } else if ($method=='POST') {
@@ -40,41 +44,10 @@ function handle_board($method,$input) {
     
 }
 
-function handle_piece($method,$input) {
-  switch ($b=array_shift($request)) {
-    case pick: if($method=='PUT'){
-                    pick_piece($input);
-               }
-               else{
-                      piece_list();
-               }
-                break;
-    case place: if($method=='GET') {
-                    show_piece($input['x'],$input['y']);
-                }
-                else if ($method=='PUT') {
-		                place_piece($input);
-                } 
-                break;
-  }
+function handle_piece($method,$x,$y,$input) {
+  
 }
 
-function handle_player($request,$method,$input) {
-	switch ($b=array_shift($request)) {
-		case '':
-		case null: if($method=='GET'){
-                                show_users($method);
-                           }
-		           else{
-                                header("HTTP/1.1 400 Bad Request"); 
-                                print json_encode(['errormesg'=>"Method $method not allowed here."]);    
-                           }
-                         break;
-                case login:  handle_user($method,$input);
-                         break; 
-                default:  header("HTTP/1.1 404 Not Found");
-			  print json_encode(['errormesg'=>"Player $b not found."]);
-                         break;
-	}
-}
+function handle_player($method,$p,$input) {}
+
  ?>
