7cafe17508a0408fb9fd5e1afe6ae071688b1c2d
xenofon <pantsosxen23@gmail.com>
2022-01-01 16:47:10 +0200

    add pieces & update css and html

--- www/js/main.js
+++ www/js/main.js
@@ -22,7 +22,29 @@ $('#cart').html(st);
 }
 function btn(){
     let s="";
+    let box=`<div id="pieces_board">
+    <h3 id="select">select piece</h3>
+   <img src="images/pieces/BCLH.jpg" class="pieces">
+   <img src="images/pieces/BCLS.jpg" class="pieces">
+   <img src="images/pieces/BCTH.jpg" class="pieces">
+   <img src="images/pieces/BCTS.jpg" class="pieces">
+   <img src="images/pieces/BSLH.jpg" class="pieces">
+   <img src="images/pieces/BSLS.jpg" class="pieces">
+   <img src="images/pieces/BSTH.jpg" class="pieces">
+   <img src="images/pieces/BSTS.jpg" class="pieces">
+   <img src="images/pieces/WCLH.jpg" class="pieces">
+   <img src="images/pieces/WCLS.jpg" class="pieces">
+   <img src="images/pieces/WCTH.jpg" class="pieces">
+   <img src="images/pieces/WCTS.jpg" class="pieces">
+   <img src="images/pieces/WSLH.jpg" class="pieces">
+   <img src="images/pieces/WSLS.jpg" class="pieces">
+   <img src="images/pieces/WSTH.jpg" class="pieces">
+   <img src="images/pieces/WSTS.jpg" class="pieces">
+   
+    
+  </div>`;
     $('#cart').html(s);
+    $('#box').html(box);
     draw_empty_board();
     fill_board();
 }
@@ -31,7 +53,7 @@ function btn(){
 
 function draw_empty_board(){
     let t='<table id="quarto_table">';
-    for (let i=1; i<5; i++){
+    for (let i=4; i>0; i--){
         t +='<tr>';
         for(let j=1; j<5; j++){
             t+='<td class="quarto_square" id="square_'+j+'_'+i+'">' + j +','+i+'</td>'; 
@@ -54,8 +76,8 @@ function fill_board_by_data(data) {
 		var id = '#square_'+ o.x +'_' + o.y;
 		var c = (o.piece!=null)?o.piece_color + o.piece:'';
 		var pc= (o.piece!=null)?'piece'+o.piece_color:'';
-		// var im = (o.piece!=null)?'<img class="piece '+pc+'" src="images/'+c+'.png">':'';
-        var im = (o.piece!=null)?c:'';
+		var im = (o.piece!=null)?'<img class="piece" src="images/pieces/'+c+'.jpg">':'';
+        // var im = (o.piece!=null)?c:'';
 		$(id).addClass(o.b_color+'_square').html(im);
 	}
  
