afbc059bf9169f32caa81fba1383f365eaa8e4c5
Andreasmeis <78660594+Andreasmeis@users.noreply.github.com>
2022-01-14 14:21:05 +0200

    🔧Update board.php

--- lib/board.php
+++ lib/board.php
@@ -28,84 +28,70 @@ function reset_board() {
 
 
 
-function move_piece($x,$y,$color,$piece,$token) {
-	
-	if($token==null || $token=='') {
-		header("HTTP/1.1 400 Bad Request");
-		print json_encode(['errormesg'=>"token is not set."]);
-		exit;
-	}
-	
-	// $player = current_player($token);
-	// if($player==null ) {
-	// 	header("HTTP/1.1 400 Bad Request");
-	// 	print json_encode(['errormesg'=>"You are not a player of this game."]);
-	// 	exit;
-	// }
-	// $status = read_status();
-	// if($status['status']!='started') {
-	// 	header("HTTP/1.1 400 Bad Request");
-	// 	print json_encode(['errormesg'=>"Game is not in action."]);
-	// 	exit;
-	// }
-	// if($status['p_turn']!=$player) {
-	// 	header("HTTP/1.1 400 Bad Request");
-	// 	print json_encode(['errormesg'=>"It is not your turn."]);
-	// 	exit;
-	// }
-	// // $orig_board=read_board();
-	// $board=read_board();
-	// $n = add_valid_moves_to_piece($x,$y);
-	// if($n==false) {
-	// 	header("HTTP/1.1 400 Bad Request");
-	// 	print json_encode(['errormesg'=>"This piece cannot be placed."]);
-	// 	exit;
-	// }
-    // else{
-    do_move($x,$y,$color,$piece);
-    //     exit;
+function move_piece($x,$y,$color,$piece,$token)
+{
+
+    if ($token == null || $token == '') {
+        header("HTTP/1.1 400 Bad Request");
+        print json_encode(['errormesg' => "token is not set."]);
+        exit;
+    }
+
+    // if($status['p_turn']!=$player) {
+    // 	header("HTTP/1.1 400 Bad Request");
+    // 	print json_encode(['errormesg'=>"It is not your turn."]);
+    // 	exit;
     // }
-
-	// header("HTTP/1.1 400 Bad Request");
-	// print json_encode(['errormesg'=>"This move is illegal."]);
-	// exit;
+    $n = add_valid_moves_to_piece($x, $y);
+    if ($n == false) {
+        header("HTTP/1.1 400 Bad Request");
+        print json_encode(['errormesg' => "This piece cannot be placed."]);
+        exit;
+    } else {
+        do_move($x, $y, $color, $piece);
+    }
 }
-function do_move($x,$y,$color,$piece) {
+    function do_move($x, $y, $color, $piece)
+    {
 
-	global $mysqli;
-	$sql = 'call `place_piece`(?,?,?,?);';
-	$st = $mysqli->prepare($sql);
-	$st->bind_param('ssss',$x,$y,$piece,$color );
-	$st->execute();
+        global $mysqli;
+        $sql = 'call `place_piece`(?,?,?,?);';
+        $st = $mysqli->prepare($sql);
+        $st->bind_param('ssss', $x, $y, $piece, $color);
+        $st->execute();
 
-	header('Content-type: application/json');
-	print json_encode(read_board(), JSON_PRETTY_PRINT);
+        header('Content-type: application/json');
+        print json_encode(read_board(), JSON_PRETTY_PRINT);
 
-}
-
-
-
-function read_board() {
-	global $mysqli;
-	$sql = 'select * from board';
-	$st = $mysqli->prepare($sql);
-	$st->execute();
-	$res = $st->get_result();
-	return($res->fetch_all(MYSQLI_ASSOC));
-}
-
-function add_valid_moves_to_piece($x,$y){
-$a=read_board();
-if($a[0]['piece']==NULL){
-    return true;
-}
-else{
-    return false;
-}
+    }
 
 
+    function read_board()
+    {
+        global $mysqli;
+        $sql = 'select * from board';
+        $st = $mysqli->prepare($sql);
+        $st->execute();
+        $res = $st->get_result();
+        return ($res->fetch_all(MYSQLI_ASSOC));
+    }
 
-}
+    function add_valid_moves_to_piece($x, $y){
+        $a = read_board();
+        for ($i = 0; $i <= 15; $i++) {
+
+            if ($a[$i]['x'] == $x) {
+                if ($a[$i]['y'] == $y) {
+                    if ($a[$i]['piece'] == NULL) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+
+        }
+    }
 
 
 ?>
